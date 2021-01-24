9a3029c1091d22980ebaae5fce1561f390e5fdf1
Harry Mills <harry@portermills.net>
2021-01-24 15:21:42 -0500

    Add Material UI lists

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,87 @@
 <script>
-  import { uniq, intersection } from 'ramda'
-  import { Alert, Row, Col } from 'svelte-materialify'
-  import Button from './Button.svelte'
-  import data from './data.json'
+  import { uniq, intersection } from "ramda";
+  import { Alert, Row, Col } from "svelte-materialify";
 
-  const vegetables = Object.keys(data)
+  import Button from "./Button.svelte";
+  import List from "./List.svelte";
+  import data from "./data.json";
 
-  let selected = []
+  const vegetables = Object.keys(data);
 
-  const handleClick = vegetable => {
+  let selected = [];
+
+  const handleClick = (vegetable) => {
     if (selected.includes(vegetable)) {
-      selected = selected.filter(veg => veg !== vegetable)
+      selected = selected.filter((veg) => veg !== vegetable);
     } else {
-      selected = [...selected, vegetable]
+      selected = [...selected, vegetable];
     }
-  }
+  };
 
-$: enemies = uniq(selected.reduce((enemies, veg) => {
-  return [...enemies, ...data[veg].enemies]
-}, []))
+  $: enemies = uniq(
+    selected.reduce((enemies, veg) => {
+      return [...enemies, ...data[veg].enemies];
+    }, [])
+  );
 
-$: allFriends = uniq(selected.reduce((friends, veg) => {
-  return [...friends, ...data[veg].friends]
-}, []))
-$: friends = allFriends.filter(friend => !enemies.includes(friend))
+  $: allFriends = uniq(
+    selected.reduce((friends, veg) => {
+      return [...friends, ...data[veg].friends];
+    }, [])
+  );
+  $: friends = allFriends.filter((friend) => !enemies.includes(friend));
 </script>
 
 <main>
   <h1>Friendly Companions</h1>
   {#each vegetables as veg}
-    <Button enemy={enemies.includes(veg)} friend={friends.includes(veg)} selected={selected.includes(veg)} handleClick={() => handleClick(veg)} {veg} />
+    <Button
+      enemy={enemies.includes(veg)}
+      friend={friends.includes(veg)}
+      selected={selected.includes(veg)}
+      handleClick={() => handleClick(veg)}
+      {veg}
+    />
   {/each}
-    <Col>
-      {#if intersection(enemies, selected).length > 0}
-        <Alert class="red-text" border="top" coloredBorder>
-          Some of the plants you've selected don't play nicely together.
-        </Alert>
-      {/if}
-  <Row>
-    <Col>
-    <h2>Friends</h2>
-    <ul>
-      {#each friends as friend}
-        <li>{friend}</li>
-      {/each}
-    </ul>
-    </Col>
-    <Col>
-    <h2>Enemies</h2>
-    <ul>
-      {#each enemies as enemy}
-        <li>{enemy}</li>
-      {/each}
-    </ul>
-    </Col>
-  </Row>
-    </Col>
+  <Col>
+    {#if intersection(enemies, selected).length > 0}
+      <Alert class="red-text" border="top" coloredBorder>
+        Some of the plants you've selected don't play nicely together.
+      </Alert>
+    {/if}
+    <Row>
+      <Col>
+        <List
+          title="Friends"
+          items={friends}
+          color="green"
+          icon="heart"
+          {selected}
+          {handleClick}
+        />
+      </Col>
+      <Col>
+        <List
+          title="Enemies"
+          items={enemies}
+          color="red"
+          icon="skull"
+          {selected}
+          {handleClick}
+        />
+      </Col>
+    </Row>
+  </Col>
 
   <h2>Your Vegetables</h2>
   <table>
     <thead>
-      <tr><th></th><th>Friends</th><th>Enemies</th></tr>
+      <tr><th /><th>Friends</th><th>Enemies</th></tr>
     </thead>
     <tbody>
       {#each selected as veg}
-        <tr><td>{veg}</td>
+        <tr
+          ><td>{veg}</td>
           <td>
             <ul>
               {#each data[veg].friends as friend}
@@ -82,6 +99,7 @@ $: friends = allFriends.filter(friend => !enemies.includes(friend))
         </tr>
       {/each}
     </tbody>
+  </table>
 </main>
 
 <style>
---
+++ src/List.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { List, Subheader, ListItem, ListItemGroup,Icon } from 'svelte-materialify'
+
+  export let handleClick
+  export let title
+  export let items
+  export let selected
+  export let icon
+  export let color
+
+$: iconClass = `mdi mdi-${icon} ${color}`
+</script>
+
+<main>
+  <List dense disabled class="elevation-2">
+    <Subheader>{title}</Subheader>
+      <ListItemGroup class="blue-text" value={selected}>
+      {#each items as item}
+        <ListItem>
+          <span slot="prepend">
+            <Icon class={iconClass} />
+          </span>
+          {item}
+        </ListItem>
+      {/each}
+    </ListItemGroup>
+  </List>
+</main>
+
+<style>
+</style>
